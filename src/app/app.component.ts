6f61eee33d0a93ee967ae73ca07fec1f9752574d
lightLook <itsyoussefmol@gmail.com>
2023-05-21 18:21:20 +0200

    modifier 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+ 
+
+
+import { Component } from '@angular/core';
+
+interface User {
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  users: User[] = [
+    {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      avatar:'https://randomuser.me/api/portraits/men/1.jpg'
+    },
+    {
+      name: 'Jane Doe',
+      email: 'jane.doe@example.com',
+      avatar: 'https://randomuser.me/api/portraits/women/2.jpg'
+    },
+    {
+      name: 'Bob Smith',
+      email: 'bob.smith@example.com',
+      avatar: 'https://randomuser.me/api/portraits/men/3.jpg'
+    }
+  ];
+}
